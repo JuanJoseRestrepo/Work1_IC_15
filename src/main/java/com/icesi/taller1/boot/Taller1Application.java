be6cd97cc761094faaf8b83b43c52c794f976f70
HalsteinKahn <callipop3@hotmail.com>
2022-06-04 15:01:51 -0500

    Zavali Ebalo :godmode: Dios ayudame este trabajo esta muy largo

--- src/main/java/com/icesi/taller1/boot/Taller1Application.java
+++ src/main/java/com/icesi/taller1/boot/Taller1Application.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 import com.icesi.taller1.model.Address;
 import com.icesi.taller1.model.Countryregion;
@@ -67,5 +68,11 @@ public class Taller1Application {
 			
 		};
 	}
+	
+	
+	 @Bean
+	 public RestTemplate getRestTemplate() {
+	    return new RestTemplate();
+	 }
 
 }
